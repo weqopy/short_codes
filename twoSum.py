0e88855e5277ddf8832ac0f8494a98e2efad067c
MK1230 <wkl231906@gmail.com>
2017-06-16 07:24:56 +0800

    LeetCode 1

---
+++ twoSum.py
@@ -0,0 +1,42 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+lt = [2, 3, 7, 0]
+target = 9
+
+
+class Good_Solution:
+    """docstring for Solution"""
+
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        if len(nums) <= 1:
+            return False
+        temp = {}
+        for i in range(len(nums)):
+            if nums[i] in temp:
+                return [temp[nums[i]], i]
+            else:
+                temp[target - nums[i]] = i
+
+
+a = Good_Solution()
+# print(a.twoSum(lt, target))
+
+
+class My_Solution:
+    """docstring for My_Solution"""
+
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype:
+        """
+        if len(nums) <= 1:
+            return False
+        pass
