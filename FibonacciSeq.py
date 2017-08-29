d0b8e779dba5f81c478490d727ff35c34638355d
weqopy <wkl231906@gmail.com>
2017-08-30 06:45:58 +0800

    Patch: PEP8 format

--- FibonacciSeq.py
+++ FibonacciSeq.py
@@ -7,8 +7,10 @@ def fib(n):
     else:
         return fib(n - 1) + fib(n - 2)
 
+
 n = int(input('Input an integer number: '))
 mod = input('Just want Nth digit? [y/n]: ')
+
 
 def output():
     if mod == 'y':
@@ -16,6 +18,7 @@ def output():
     elif mod == 'n':
         for i in range(1, n + 1):
             print(fib(i))
+
 
 if __name__ == '__main__':
     output()
