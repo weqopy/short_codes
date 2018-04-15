1274ef8e77c726776c9e579e2d5a2d1eb8073a79
weqopy <weqopy@gmail.com>
2018-04-15 12:23:19 +0800

    FibonacciSeq Update

--- FibonacciSeq.py
+++ FibonacciSeq.py
@@ -1,24 +1,37 @@
+from functools import wraps
+
+
+def memo(func):
+    cache = {}
+
+    @wraps(func)
+    def wrap(*args):
+        if args not in cache:
+            cache[args] = func(*args)
+        return cache[args]
+
+    return wrap
+
+
+# 递归
+@memo
 def fib(n):
-    assert n > 0
-    if n == 1:
-        return 1
-    elif n == 2:
+    if n <= 2:
         return 1
     else:
         return fib(n - 1) + fib(n - 2)
 
 
-n = int(input('Input an integer number: '))
-mod = input('Just want Nth digit? [y/n]: ')
+# 迭代
+def fib_2(n):
+    if n <= 2:
+        return 1
+    a, b = 1, 1
+    while n > 2:
+        a, b = b, a + b
+        n -= 1
+    return b
 
 
-def output():
-    if mod == 'y':
-        print(fib(n))
-    elif mod == 'n':
-        for i in range(1, n + 1):
-            print(fib(i))
-
-
-if __name__ == '__main__':
-    output()
+print(fib(4))
+print(fib_2(4))
