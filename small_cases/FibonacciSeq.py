cfe5a26ee8c3559052dee5febf250ba5093f8d69
weqopy <weqopy@gmail.com>
2018-05-28 21:11:55 +0800

    update first cardinal number

--- small_cases/FibonacciSeq.py
+++ small_cases/FibonacciSeq.py
@@ -16,7 +16,9 @@ def memo(func):
 # 递归
 @memo
 def fib(n):
-    if n <= 2:
+    if n == 1:
+        return 0
+    elif n == 2:
         return 1
     else:
         return fib(n - 1) + fib(n - 2)
@@ -24,14 +26,16 @@ def fib(n):
 
 # 迭代
 def fib_2(n):
-    if n <= 2:
+    if n == 1:
+        return 0
+    elif n == 2:
         return 1
-    a, b = 1, 1
+    a, b = 0, 1
     while n > 2:
         a, b = b, a + b
         n -= 1
     return b
 
 
-print(fib(4))
-print(fib_2(4))
+print(fib(10))
+print(fib_2(10))
