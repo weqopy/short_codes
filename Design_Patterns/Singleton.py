f9d69eb860d08710bc1aa85c1b5f71aa95697cbc
weqopy <weqopy@gmail.com>
2019-01-05 19:34:23 +0800

    update singleton

--- Design_Patterns/Singleton.py
+++ Design_Patterns/Singleton.py
@@ -10,16 +10,12 @@ class Singleton:
         return "Singleton module"
 
 
-from functools import wraps
-
-
 def singleton(cls):
     instances = {}
 
-    @wraps(cls)
-    def get_instance(*args, **kwargs):
+    def get_instance(*args, **kw):
         if cls not in instances:
-            instances[cls] = cls(*args, **kwargs)
+            instances[cls] = cls(*args, **kw)
         return instances[cls]
 
     return get_instance
