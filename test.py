32d119a0cbfbe298f0e4dd74a9f24460a835c609
weqopy <weqopy@gmail.com>
2020-12-09 12:41:20 +0800

    selenium save html temp

--- test.py
+++ test.py
@@ -1,9 +1,21 @@
-print(3333)
-print(2)
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 
 
-def func(name):
-    print(name)
-
-
-func("adfda")
+try:
+    browser = webdriver.Chrome()
+    browser.get('http://www.baidu.com/')
+    time.sleep(1)
+    print("opened...")
+    ActionChains(browser).key_down(Keys.COMMAND).send_keys('s').key_up(Keys.COMMAND).perform()
+    # browser.find_element_by_id('body').send_keys(Keys.COMMAND,'s')
+    time.sleep(1)
+    ActionChains(browser).send_keys(Keys.ENTER).perform()
+    # browser.find_element_by_id('body').send_keys(Keys.ENTER)
+    time.sleep(1)
+except Exception as e:
+    raise e
+finally:
+    browser.quit()
