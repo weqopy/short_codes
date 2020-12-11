66e2c12151999506391001f03cebe0edd6a8669e
weqopy <weqopy@gmail.com>
2020-12-11 21:44:44 +0800

    pyautogui

--- test.py
+++ test.py
@@ -1,21 +1,18 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
+from helium import *
+import pyautogui
 
+
+pyautogui.PAUSE = 2
 
 try:
-    browser = webdriver.Chrome()
-    browser.get('http://www.baidu.com/')
-    time.sleep(1)
-    print("opened...")
-    ActionChains(browser).key_down(Keys.COMMAND).send_keys('s').key_up(Keys.COMMAND).perform()
-    # browser.find_element_by_id('body').send_keys(Keys.COMMAND,'s')
-    time.sleep(1)
-    ActionChains(browser).send_keys(Keys.ENTER).perform()
-    # browser.find_element_by_id('body').send_keys(Keys.ENTER)
-    time.sleep(1)
+    start_chrome("http://www.baidu.com/")
+    pyautogui.hotkey('command', 's')
+    pyautogui.write('ood')
+    pyautogui.hotkey('enter')
 except Exception as e:
     raise e
 finally:
-    browser.quit()
+    kill_browser()
