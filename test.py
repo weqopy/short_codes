a6557a4c1f4cabe73c73a3120245f12186e3a9fc
weqopy <weqopy@gmail.com>
2021-06-15 10:00:59 +0800

    up test

--- test.py
+++ test.py
@@ -1,18 +1,4 @@
-import time
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from helium import *
-import pyautogui
+import requests
 
-
-pyautogui.PAUSE = 2
-
-try:
-    start_chrome("http://www.baidu.com/")
-    pyautogui.hotkey('command', 's')
-    pyautogui.write('ood')
-    pyautogui.hotkey('enter')
-except Exception as e:
-    raise e
-finally:
-    kill_browser()
+res = requests.get("http://www.baidu.com/")
+print(res.text)
