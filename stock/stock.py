41117d243c01ab37b2aaacda2bdf89e27b019341
weqopy <weqopy@gmail.com>
2021-10-23 18:13:03 +0800

    up stock

--- stock/stock.py
+++ stock/stock.py
@@ -10,30 +10,26 @@ print("login respond  error_msg:" + lg.error_msg)
 
 #### 获取历史K线数据 ####
 # 详细指标参数，参见“历史行情指标参数”章节
-columns = "code,open,high,low,close,turn,pctChg"
+columns = "code,date,open,high,low,close,turn,pctChg"
 data_list = []
 with open("stock/stock_cs.csv") as file:
     data = file.readlines()
     for line in data:
-        # time.sleep(0.01)
+        time.sleep(0.01)
         number, type = line.strip().split(".")
         code = (".").join([type.lower(), number])
         rs = bs.query_history_k_data_plus(
             code,
             columns,
-            start_date="2021-07-20",
-            end_date="2021-07-20",
+            start_date="2021-10-18",
+            end_date="2021-10-20",
         )
 
-        i = 0
-        while rs.error_code == "0" and rs.next():
-            # 获取一条记录，将记录合并在一起
-            if i == 0:
-                i = 1
-                continue
-            data_list.append(rs.get_row_data())
-            # print(rs.fields)
         print(code)
+        while rs.error_code == "0" and rs.next():
+            row = rs.get_row_data()
+            print(row)
+            data_list.append(row)
 # print(data_list)
 result = pd.DataFrame(
     data_list,
